fec7a778a8f7f534e7fffdf372a634d2635810e2
Rahul Yedida <rahul.yedida@pm.me>
2022-04-22 16:38:14 -0400

    Implemented adding reviews

---
+++ server/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, Response
+import pymongo
+import dotenv
+import json
+import urllib
+
+app = Flask(__name__)
+
+# PyMongo setup
+config = dotenv.dotenv_values('.env')
+username = config['USERNAME']
+password = config['PASSWORD']
+db = config['DB']
+url = config['URL']
+conn_url = f'mongodb+srv://{urllib.parse.quote(username)}:{urllib.parse.quote(password)}@{url}/{db}?retryWrites=true&w=majority'
+client = pymongo.MongoClient(conn_url)
+db = client.rmp
+
+@app.route('/api/reviews/submit', methods=['POST'])
+def submit_review():
+    req = request.get_json(force=True)
+    university = req['university']
+    program = req['program']
+    rating = req['rating']
+    review = req['review']
+    degree = req['degree']
+
+    reviews = db.reviews
+    success = reviews.insert_one({
+        'university': university,
+        'program': program,
+        'rating': rating,
+        'review': review,
+        'degree': degree
+    }).acknowledged
+
+    if success:
+        return Response(json.dumps({'status': 'ok'}), status=200, headers={'Content-Type': 'application/json'})
+    else:
+        return Response(json.dumps({'status': 'error'}), status=500, headers={'Content-Type': 'application/json'})
+
+@app.route('/*', methods=['POST'])
+def catch_all():
+    print(request.json)
--- src/NewReview/NewReview.js
+++ src/NewReview/NewReview.js
@@ -1,5 +1,38 @@
 import './NewReview.sass';
 
+function submitReview() {
+    // Process bad rating values
+    let rating = document.getElementById('new-rating').value;
+    if (rating < 1)
+        rating = 1;
+    if (rating > 5)
+        rating = 5;
+
+    const review = {
+        university: document.getElementById('new-university').value,
+        program: document.getElementById('new-program').value,
+        degree: document.getElementById('new-degree').value,
+        rating: document.getElementById('new-rating').value,
+        review: document.getElementById('new-review').value
+    };
+
+    fetch('http://127.0.0.1:5000/api/reviews/submit', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(review),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
 export default function NewReview() {
     return (
         <div className="new-review">
@@ -8,8 +41,9 @@ export default function NewReview() {
                 <input id="new-university" type="text" placeholder="University" />
                 <input id="new-degree" type="text" placeholder="Degree (MS / PhD / etc.)" />
                 <input id="new-program" type="text" placeholder="Program (e.g. Computer Science)" />
+                <input id="new-rating" type="number" min={1} max={5} placeholder="Rating (1-5)" />
                 <textarea id="new-review" rows={10} placeholder="Review" />
-                <button>Submit</button>
+                <button onClick={submitReview}>Submit</button>
             </div>
             <div className="column">
                 <h2>Find reviews.</h2>
